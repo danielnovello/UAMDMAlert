6c2e738fd5fef0f94f9576d7ee9dd40e3d1d53b7
Daniel Novello <djquazzi@gmail.com>
2020-05-06 10:43:11 +0200

    Initial Commit

---
+++ UAMDMAlert/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UAMDMAlert
+//
+//  Created by Daniel Novello on 2020/05/06.
+//  Copyright © 2020 Daniel Novello. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
